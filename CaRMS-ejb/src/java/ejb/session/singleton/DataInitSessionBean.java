1510daacf93867e0cb10afa758352d94d416d8bd
Nevan Ng <ngnevan@gmail.com>
2022-10-14 23:51:21 +0800

    initial commit

---
+++ CaRMS-ejb/src/java/ejb/session/singleton/DataInitSessionBean.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejb.session.singleton;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.Singleton;
+import javax.ejb.LocalBean;
+import javax.ejb.Startup;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author nevanng
+ */
+@Singleton
+@LocalBean
+@Startup
+public class DataInitSessionBean {
+
+    @PersistenceContext(unitName = "CaRMS-ejbPU")
+    private EntityManager em;
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+    
+    @PostConstruct
+    public void postConstruct() {
+        
+    }
+
+}
---
+++ CaRMSLibrary/src/entity/Customer.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author nevanng
+ */
+@Entity
+public class Customer implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long custId;
+
+    public Long getCustId() {
+        return custId;
+    }
+
+    public void setCustId(Long custId) {
+        this.custId = custId;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (custId != null ? custId.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the custId fields are not set
+        if (!(object instanceof Customer)) {
+            return false;
+        }
+        Customer other = (Customer) object;
+        if ((this.custId == null && other.custId != null) || (this.custId != null && !this.custId.equals(other.custId))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entity.Customer[ id=" + custId + " ]";
+    }
+    
+}
